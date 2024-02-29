a4b186b805767ba0151daacab58f7a5713d416dd
DiabolusGX <ankit.malik1092@gmail.com>
2024-03-01 02:17:15 +0530

    host

--- server/index.js
+++ server/index.js
@@ -14,21 +14,26 @@ const database = require("./database");
 // app setup
 const app = express();
 
+// Trust the first hop, useful if your Node.js app is behind a single proxy
+app.set("trust proxy", 1);
+
 // Helmet without contentSecurityPolicyset to false
 // blocks the react frontend from loading for some reason.
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(express.json());
-app.use(sessions({
-    cookieName: "session", // cookie name dictates the key name added to the request object
-    secret: process.env.secret, // should be a large unguessable string
-    duration: 1000 * 60 * 60 * 24, // how long the session will stay valid in ms
-    activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
-    cookie: {
-        httpOnly: true, // Cookie is not accessible from javascript
-        ephemeral: true, // Exit session when browser closes
-        secure: false // Only allow through SSL
-    }
-}));
+app.use(
+    sessions({
+        cookieName: "session", // cookie name dictates the key name added to the request object
+        secret: process.env.secret, // should be a large unguessable string
+        duration: 1000 * 60 * 60 * 24, // how long the session will stay valid in ms
+        activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+        cookie: {
+            httpOnly: true, // Cookie is not accessible from javascript
+            ephemeral: true, // Exit session when browser closes
+            secure: false, // Only allow through SSL
+        },
+    })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -36,7 +41,7 @@ app.use(passport.session());
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100,
-    message: "Enter the chill zone."
+    message: "Enter the chill zone.",
 });
 app.use("/api/v1/", apiLimiter);
 
